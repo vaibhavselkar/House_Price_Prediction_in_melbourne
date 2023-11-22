e5e7389430e3c1ff62e896b98effdda4000669ba
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2023-11-22 17:19:29 +0530

    Add files via upload

---
+++ interactive_app.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
+from category_encoders import OneHotEncoder
+import streamlit as st
+import pydeck as pdk
+
+# Load your dataset (replace 'path/to/your/dataset.csv' with the actual path)
+df = pd.read_csv("melb_data.csv")
+
+# Clearing outliers in BuildingArea by taking out the top 5 percent and lower 5 percent
+low, high = df["BuildingArea"].quantile([0.05, 0.95])
+mask_area = df["BuildingArea"].between(low, high)
+df = df[mask_area]
+
+# Removing outlier from prices
+Q1 = df["Price"].quantile(0.25)
+Q3 = df["Price"].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define the lower and upper bounds for outliers
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# Filter the DataFrame to exclude outliers
+df = df[(df["Price"] >= lower_bound) & (df["Price"] <= upper_bound)]
+
+# Features and target
+target = "Price"
+features = ["BuildingArea", "Lattitude", "Longtitude"]
+X = df[features]
+y = df[target]
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and train the model
+model = make_pipeline(
+    OneHotEncoder(use_cat_names=True),
+    Ridge()
+)
+model.fit(X_train, y_train)
+
+# Streamlit app
+st.title("Apartment Price Prediction In Melbourne")
+
+# Sidebar with user input
+st.sidebar.header("User Input")
+building_area = st.sidebar.slider("Building Area [m2]", X["BuildingArea"].min(), X["BuildingArea"].max(), X["BuildingArea"].mean())
+latitude = st.sidebar.slider("Latitude", X["Lattitude"].min(), X["Lattitude"].max(), X["Lattitude"].mean())
+longitude = st.sidebar.slider("Longitude", X["Longtitude"].min(), X["Longtitude"].max(), X["Longtitude"].mean())
+
+# Button to trigger prediction
+if st.sidebar.button("Predict"):
+    # Make predictions
+    prediction = model.predict([[building_area, latitude, longitude]])
+    prediction = round(prediction[0], 2)
+
+    # Display prediction
+    st.subheader("Prediction")
+    st.write(f"The predicted apartment price is ${prediction}")
+
+    # Display the map with the selected location
+    # Create a PyDeck scatterplot layer for the map
+    scatterplot = pdk.Layer(
+        "ScatterplotLayer",
+        data=pd.DataFrame({"latitude": [latitude], "longitude": [longitude]}),
+        get_position="[longitude, latitude]",
+        get_radius=100,
+        get_fill_color=[255, 0, 0],  # Red color
+    )
+
+    # Create a PyDeck map using the scatterplot layer
+    deck = pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state=pdk.ViewState(latitude=latitude, longitude=longitude, zoom=12, pitch=50),
+        layers=[scatterplot],
+    )
+
+    # Display the PyDeck chart using st.pydeck_chart
+    st.pydeck_chart(deck)
